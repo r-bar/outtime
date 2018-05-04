18c3df072087136866b56b374d0bb2f743c25345
Ryan Barth <ryan@barth.tech>
2018-05-03 19:34:02 -0700

    add some command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,77 @@
 extern crate clap;
 
 use std::io::stdin;
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
+use clap::{App, Arg};
 
-fn done(start_time: &SystemTime) {
-    let elapsed = start_time.elapsed().unwrap();
-    let secs = elapsed.as_secs();
-    let micros = elapsed.subsec_nanos();
-    println!("\nDone! Total elapsed: {}.{:09}", secs, micros);
+fn command_line_parser() -> App<'static, 'static> {
+    return App::new("Output benchmarking program")
+        .version("0.1")
+        .about("Times the output of a program for rough benchmarking of simple scripts.")
+        .arg(Arg::with_name("diff")
+             .short("d")
+             .long("diff")
+             .help("Instead of displaying the total time elaped, shows the time elapsed since the previous line of output.")
+        )
 }
 
-fn output_time(start_time: &SystemTime, line: &str) {
-    let elapsed = start_time.elapsed().unwrap();
-    let secs = elapsed.as_secs();
-    let micros = elapsed.subsec_nanos();
-    println!("{}.{:09} | {}", secs, micros, line.trim_right());
+fn fmt_duration(duration: Duration) -> String {
+    let secs = duration.as_secs();
+    let micros = duration.subsec_nanos();
+    format!("{}.{:09}", secs, micros)
+}
+
+struct Timer {
+    start_time: SystemTime,
+    last_output_time: Option<SystemTime>,
+}
+
+impl Timer {
+    fn new() -> Timer {
+        Timer {
+            start_time: SystemTime::now(),
+            last_output_time: None,
+        }
+    }
+
+    fn diff_elasped(&mut self) -> Duration {
+        let duration = match self.last_output_time {
+            Some(last_time) => last_time.elapsed(),
+            None => self.start_time.elapsed()
+        };
+        self.last_output_time = Some(SystemTime::now());
+        duration.unwrap()
+    }
+
+    fn elapsed(&self) -> Duration {
+        self.start_time.elapsed().unwrap()
+    }
 }
 
 fn main() {
-    let mut more: bool = true;
+    let cli = command_line_parser();
+    let matches = cli.get_matches();
     let input = stdin();
-    let start_time = SystemTime::now();
-    while more {
+    let mut timer = Timer::new();
+    loop {
         let mut line = String::new();
         match input.read_line(&mut line) {
             Ok(0) => {
-                more = false;
-                done(&start_time);
+                let elapsed = timer.elapsed();
+                println!("\nDone! Total elapsed: {}", fmt_duration(elapsed));
+                break;
             }
-            Err(_) => {
-                more = false;
-                done(&start_time);
+            Err(e) => {
+                eprintln!("Error reading input: {}", e);
+                break;
             }
             Ok(_) => {
-                output_time(&start_time, &line);
+                let elapsed = if matches.is_present("diff") {
+                    timer.diff_elasped()
+                } else {
+                    timer.elapsed()
+                };
+                println!("{} | {}", fmt_duration(elapsed), line.trim_right())
             }
         }
     }
